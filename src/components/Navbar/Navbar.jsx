427979f9907808f576d40fe96c53eb837ea61e1a
kalyan <kalya@gmail.com>
2026-07-05 23:19:05 +0530

    update portfoliio

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,47 @@
+import { FaBars, FaTimes } from "react-icons/fa";
+import { useState, useEffect } from "react";
 import "./Navbar.css";
 
 function Navbar() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    if (menuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [menuOpen]);
+
   return (
     <nav className="navbar">
-      <div className="logo">Kalyan Kumar Miriyala</div>
 
-      <ul className="nav-links">
-        <li><a href="#hero">Hero</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#skills">Skills</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
+      <div className="logo">
+        Kalyan Kumar Miriyala
+      </div>
+
+      <ul className={menuOpen ? "nav-links active" : "nav-links"}>
+
+        <li><a href="#hero" onClick={() => setMenuOpen(false)}>Home</a></li>
+        <li><a href="#about" onClick={() => setMenuOpen(false)}>About</a></li>
+        <li><a href="#skills" onClick={() => setMenuOpen(false)}>Skills</a></li>
+        <li><a href="#projects" onClick={() => setMenuOpen(false)}>Projects</a></li>
+        <li><a href="#contact" onClick={() => setMenuOpen(false)}>Contact</a></li>
+
       </ul>
+
+      <div
+        className="menu-icon"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
     </nav>
   );
 }
